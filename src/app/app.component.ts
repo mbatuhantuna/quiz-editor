7877006166fe719ea684456c68d8a9e7ec7dd464
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-02-06 20:44:16 -0600

    Creating Quiz Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'quiz-editor';
+  title = 'Quiz Editor';
+
+  quizzes = [
+    {
+      name: 'Quiz 1',
+      showDelete: false,
+    },
+
+    {
+      name: 'Quiz 2',
+      showDelete: false,
+    },
+
+    {
+      name: 'Quiz 3',
+      showDelete: false,
+    },
+  ];
+
+
+addFunnyQuiz = () => {
+  this.quizzes = [
+    ...this.quizzes
+    , {
+      name: 'Funny Quiz'
+      , showDelete: true
+    }
+  ];
+};
+
+deleteQuiz = (q:any) => {
+  this.quizzes = this.quizzes.filter(x => x !== q );
+};
+
+addQuiz  = () => {
+  this.quizzes = [
+    ...this.quizzes
+    , {
+      name: this.newQuizName
+      ,showDelete:false
+    }
+  ];
+  this.newQuizName = '';
+};
+
+newQuizName = '';
+
 }
